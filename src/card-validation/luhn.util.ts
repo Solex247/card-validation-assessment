3ce4f4ad11c95c1365aec8ea39458446b40fdc4c
Solomon Adams <solex247@gmail.com>
2026-09-02 08:38:44 +0100

    feat: implement Luhn algorithm for card validation

---
+++ src/card-validation/luhn.util.ts
@@ -0,0 +1,17 @@
+export function isValidLuhn(cardNumber: string): boolean {
+  const digits = cardNumber.split('').map(Number);
+  let sum = 0;
+  let shouldDouble = false;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = digits[i];
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  return sum % 10 === 0;
+}
